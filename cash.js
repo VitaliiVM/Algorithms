cff9eac6ca92b3a4604685845bd95cc91653a300
VitaliiVM <69699528+VitaliiVM@users.noreply.github.com>
2021-04-21 11:48:12 +0300

    2 algorithms

---
+++ cash.js
@@ -0,0 +1,33 @@
+
+
+function cashFunction (fn) {
+ const cash = {};
+ return function (n) {
+     if (cash[n]) {
+         console.log('Взято из кеша', cash[n]);
+         return cash[n];
+     }
+     let result = fn(n);
+     console.log('Посчитала функция = ', result);
+     cash[n] = result;
+     return result;
+ };
+}
+
+function  fuctorial(n) {
+    let result = 1;
+    while (n != 1) {
+        result *= n;
+        n -= 1;
+    }
+    return result;
+}
+
+const cashFactorial = cashFunction(fuctorial);
+cashFactorial(5);
+cashFactorial(4);
+cashFactorial(3);
+cashFactorial(3);
+cashFactorial(5);
+
+
